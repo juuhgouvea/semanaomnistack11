d652be39497777943a284b8b7df76e6ed2d6b245
juuhgouvea <juliasantos752@yahoo.com.br>
2020-03-26 21:46:16 -0300

    Mobile da aplicação

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -14,7 +14,9 @@ export default function Logon() {
     e.preventDefault();
 
     try {
-      await api.post("sessions", { id });
+      const response = await api.post("sessions", { id });
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
 
       history.push("/profile");
     } catch (err) {
